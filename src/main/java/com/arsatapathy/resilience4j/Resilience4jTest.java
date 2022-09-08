e7cfadc18cba2a220f22810a71da60a75248352e
Ashish Satapathy <ashish.satapathy2014@gmail.com>
2022-09-08 08:44:30 +0530

    Resilience4j

--- src/main/java/com/arsatapathy/resilience4j/Resilience4jTest.java
+++ src/main/java/com/arsatapathy/resilience4j/Resilience4jTest.java
@@ -4,29 +4,49 @@ import io.github.resilience4j.timelimiter.TimeLimiter;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class Resilience4jTest {
 
     public static void main(String[] args) {
-        TimeLimiterConfig config = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(5)).build();
+        TimeLimiterConfig config = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(1)).build();
         TimeLimiter timeLimiter = TimeLimiter.of("My Limiter", config);
 
         ExecutorService executorService = Executors.newFixedThreadPool(1);
 
-        Callable<String> callable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> executorService.submit(() -> {
-            System.out.println("executing...");
-            Thread.sleep(4000);
-            return "ashish";
-        }));
+        List<Integer> integers = new ArrayList<>();
 
-        try {
-            System.out.println(callable.call());
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (int i = 0; i < 10; i++) {
+            integers.add(i);
         }
+
+        List<Callable<String>> callables = integers.stream().map(
+                i -> TimeLimiter.decorateFutureSupplier(timeLimiter, () ->
+                        executorService.submit(
+                                () -> {
+                                    System.out.println("executing..." + i);
+                                    if (i == 5) {
+                                        Thread.sleep(1000000000000000000L);
+                                    }
+                                    System.out.println("finished execution..." + i);
+                                    return "ashish" + i;
+                                }
+                        )
+                )
+        ).collect(Collectors.toList());
+
+        callables.forEach(c -> {
+            try {
+                System.out.println("got : " + c.call());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
         executorService.shutdown();
     }
