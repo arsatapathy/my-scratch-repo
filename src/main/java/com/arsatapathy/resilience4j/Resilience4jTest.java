3e29c4dac48459934b6e594b34f451f5326764ec
Ashish Satapathy <ashish.satapathy2014@gmail.com>
2022-09-08 10:15:06 +0530

    Resilience4j

--- src/main/java/com/arsatapathy/resilience4j/Resilience4jTest.java
+++ src/main/java/com/arsatapathy/resilience4j/Resilience4jTest.java
@@ -4,9 +4,12 @@ import io.github.resilience4j.timelimiter.TimeLimiter;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
@@ -14,10 +17,10 @@ import java.util.stream.Collectors;
 public class Resilience4jTest {
 
     public static void main(String[] args) {
-        TimeLimiterConfig config = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(1)).build();
+        TimeLimiterConfig config = TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(2)).build();
         TimeLimiter timeLimiter = TimeLimiter.of("My Limiter", config);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
         List<Integer> integers = new ArrayList<>();
 
@@ -27,27 +30,43 @@ public class Resilience4jTest {
 
         List<Callable<String>> callables = integers.stream().map(
                 i -> TimeLimiter.decorateFutureSupplier(timeLimiter, () ->
-                        executorService.submit(
+                        CompletableFuture.supplyAsync(
                                 () -> {
-                                    System.out.println("executing..." + i);
+                                    System.out.println("executing " + i + " at " + df.format(LocalDateTime.now()));
                                     if (i == 5) {
-                                        Thread.sleep(1000000000000000000L);
+                                        try {
+                                            Thread.sleep(1000000000000000000L);
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                    } else {
+                                        try {
+                                            Thread.sleep(1000L);
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
                                     }
-                                    System.out.println("finished execution..." + i);
-                                    return "ashish" + i;
+                                    System.out.println("finished execution of " + i + " at " + df.format(LocalDateTime.now()));
+                                    return "ashish " + i;
                                 }
                         )
                 )
         ).collect(Collectors.toList());
 
-        callables.forEach(c -> {
-            try {
-                System.out.println("got : " + c.call());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        ExecutorService ex = Executors.newFixedThreadPool(5);
 
-        executorService.shutdown();
+        callables.forEach(
+                c ->
+                        ex.execute(() -> {
+                                    try {
+                                        System.out.println("got " + c.call() + " at " + df.format(LocalDateTime.now()));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                        )
+        );
+
+        ex.shutdown();
     }
 }
